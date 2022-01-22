4fac3500a67b57dc44219236710ba323baf15281
daniel <danlikestech@gmail.com>
2022-01-22 14:32:10 -0600

    added a config file for mongoose & started route folder

---
+++ config.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const { config } = require("dotenv");
+const mongoose = require("mongoose");
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URI, {
+    useNewURLParser: true,
+  });
+  await mongoose.connection.once("open", () => {
+    console.log("Connected");
+  });
+}
+
+module.exports = main;
+config.js;
--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoConnection = require("./config");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 
 const app = express();
-
 const port = 3000 || process.env.PORT;
+
+// Security
+app.use(helmet());
+app.use(bodyParser.json());
+// Security
+
+dotenv.config();
+app.use(morgan("dev"));
+
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Base Route is up" });
 });
 
 app.listen(port, () => {
+  mongoConnection();
   console.log(`Server is listening at ${port}`);
 });
