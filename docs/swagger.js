60b784655f1382edea15b9496aed43c0051f69d6
daniel <danlikestech@gmail.com>
2022-01-28 10:27:52 -0600

    setup routes for swagger API

--- docs/swagger.js
+++ docs/swagger.js
@@ -2,27 +2,52 @@ module.exports = {
   openapi: "3.0.0",
   info: {
     title: "Blog Api",
-    description: "Welcome to my blog",
+    description: "Dan's Blog API",
     version: "0.1.9",
   },
   paths: {
-    "/auth": {
+    "/users": {
       get: {
-        summary: "",
+        summary: "Gets all users",
       },
     },
     "/auth/register": {
       post: {
-        summary: "",
+        summary: "Register a user",
+        parameters: [
+          {
+            name: "newUser",
+            in: "body",
+            schema: {},
+            required: true,
+            description: "registers a user",
+          },
+        ],
       },
     },
     "/auth/login": {
       post: {
-        summary: "",
+        summary: "Login a user",
       },
     },
-    "/blogs": {
+    "/blog": {
       get: {
+        summary: "Gets a blog from a user",
+      },
+    },
+    "/blog/private": {
+      get: {
+        summary: "Gets only the private blogs",
+      },
+    },
+    "/blog/update/id": {
+      put: {
+        summary: "Updates the id",
+      },
+    },
+
+    "/blog/delete/id": {
+      delete: {
         summary: "",
       },
     },
